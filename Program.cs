ba84e333df6c9f8e037ac830313a5f8596942561
Israel Ziri <oladejoisreal274@gmail.com>
2023-11-15 16:06:39 +0100

    vs code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Bored
+{
+    internal class Program
+    {
+
+        private int lol;
+
+        public Program(int lol)
+        {
+            this.lol = lol;
+        }
+
+        public int getLol()
+        {
+            return lol;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
